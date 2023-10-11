0dfac75b21a7e0a11b4a5e82d5eebc31bba29195
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-10-11 12:00:00 +0530

    file added

---
+++ 122-Buy-sell-stocks-II.js
@@ -0,0 +1,20 @@
+// Creating a function stocks and calculating the maximum profit
+
+function stocks(prices){
+    let i = 0; //initializing the value of i
+    let totalProfit = 0;
+    let profit = 0;
+
+      while(i<prices.length){ //i must be lesser than the length of prices[]
+        let j = i+1;  // initializing j here because we want to increament both i and j at the same time.
+            if(prices[i] < prices[j]){
+                profit = prices[j] - prices[i];
+                totalProfit = totalProfit + profit; //if the condition is true it will calculate this and keep the total value.
+            }
+        i++; // increamenting i here (code will go back on line 8 from here until the value of i is less than length of prices)
+      }
+       
+      return totalProfit;// returning the final totalprofit
+}
+
+console.log(stocks([7,1,5,3,6,4]));// Calling the function stocks()
