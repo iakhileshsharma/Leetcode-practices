feb813a5b4b080747da9b1006220c59b95743429
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-10-23 12:00:00 +0530

    file added

--- 1-Two-sum.js
+++ 1-Two-sum.js
@@ -2,8 +2,8 @@
 // For example we can assume nums = [2,7,11,15], target = 9
  function twoSum(nums, target) {
     // looping through the nums with i and j(where j will be always +1 than i)
-    for(let i = 0; i<nums.length; i++){
-        for(let j = i+1; j<nums.length; j++){
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
             const finalnum = nums[i] + nums[j];//adding the value of nums and storing it in finalnum variable.
             
             if(finalnum === target) //Giving a condition for checking if the finalnum is equal to the given tagret or not
@@ -11,5 +11,5 @@
             return [i,j]; // Returnng the value of i and j (indexes only)
         }
     }
-};
+}
 
