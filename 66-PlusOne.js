ccfc715740f1830cff0627c8d8c9d08789575519
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-10-28 12:00:00 +0530

    file added

---
+++ 66-PlusOne.js
@@ -0,0 +1,19 @@
+function PlusOne(digits){
+    // Using BigInt for Big numbers, so we can join it
+    let num = BigInt(digits.join(""));
+    let sum = num + BigInt(1);
+    // Converting the whole sum of number into string and splliting it
+    let result = sum.toString().split("");
+    
+    // Using (for loop) to iterate on every index(result[i]) to make it int
+    for(let i=0; i<result.length; i++){
+        result[i] = parseInt(result[i]);
+    }
+        
+ 
+    console.log(result);
+
+
+}
+PlusOne([1,2,3]); 
+
