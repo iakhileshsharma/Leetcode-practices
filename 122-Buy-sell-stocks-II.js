7e49a514fba97b620f1e208204e9d149901b8fbb
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-10-29 12:00:00 +0530

    file added

--- 122-Buy-sell-stocks-II.js
+++ 122-Buy-sell-stocks-II.js
@@ -1,17 +1,17 @@
-// Creating a function stocks and calculating the maximum profit
+// Creating a function stocks and calculating the maximum profit.(Meduim level)
 
 function stocks(prices){
-    let i = 0; //initializing the value of i
+    let i = 0; // Initializing the value of i
     let TotalProfit = 0;
     let profit = 0;
 
-      while(i<prices.length){ //i must be lesser than the length of prices[]
+      while(i<prices.length){ // i must be lesser than the length of prices[]
         let j = i+1;  // initializing j here because we want to increament both i and j at the same time.
             if(prices[i] < prices[j]){
                 profit = prices[j] - prices[i];
                 TotalProfit = TotalProfit + profit; //if the condition is true it will calculate this and keep the total value.
             }
-        i++; // increamenting i here (code will go back on line 8 from here until the value of i is less than length of prices)
+        i++; // Increamenting i here (code will go back on line 8 from here until the value of i is less than length of prices)
       }
        
       return TotalProfit;// returning the final totalprofit
