a377bf2ebeeff18c6e9e05c1d0b5e961130f86c5
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-04 12:00:00 +0530

    file added

--- 414-Third-max-number.js
+++ 414-Third-max-number.js
@@ -1,11 +1,20 @@
 // Creating a function thirdMax() and returning the third distinct maximum number in an array(nums[]).
-// Input = [3,2,1]
-// Output = 1
+// input = [2,2,3,1]
+// 
 
 function thirdMax(nums){
-    nums.sort();
-    
+  nums.sort(function (a, b) {
+    return a-b;
+  }).reverse();
+
+  let newArr = [...new Set(nums)];
+
+    if(newArr.length < 3){
+        return newArr[0];
+    }else{
+        return newArr[2];
+    }
     
     
 }
-console.log(thirdMax([3,2,1]));
+console.log(thirdMax([3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6]));
