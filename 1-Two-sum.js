722bc49564a5a8b9c064c261ed316ebdc71bee05
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-14 12:00:00 +0530

    file added

--- 1-Two-sum.js
+++ 1-Two-sum.js
@@ -1,8 +1,12 @@
 var twoSum = function(nums, target) {
+    
     for(let i = 0; i<nums.length; i++){
         for(let j = i+1; j<nums.length; j++){
             const finalnum = nums[i] + nums[j];
-            if(finalnum === target)return [i,j];
+            
+            if(finalnum === target)
+            
+            return [i,j];
         }
     }
 };
