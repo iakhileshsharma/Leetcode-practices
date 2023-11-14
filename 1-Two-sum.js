9ccadbec33b2052a0459cace28b35be9dd5cb5df
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-14 12:00:00 +0530

    file added

--- 1-Two-sum.js
+++ 1-Two-sum.js
@@ -1,5 +1,5 @@
-// creating a function to check the sum of two elements in an array equals to the given target.
-
+// Creating a function to check the sum of two elements in an array equals to the given target.
+// For example we can assume nums = [2,7,11,15], target = 9
 var twoSum = function(nums, target) {
     // looping through the nums with i and j(where j will be always +1 than i)
     for(let i = 0; i<nums.length; i++){
@@ -8,7 +8,7 @@ var twoSum = function(nums, target) {
             
             if(finalnum === target)
             
-            return [i,j]; // returnng the value of i and j (indexes only)
+            return [i,j]; // Returnng the value of i and j (indexes only)
         }
     }
 };
