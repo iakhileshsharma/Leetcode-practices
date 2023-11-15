23b4794a7892d19548ca65f9b601bb765e66a65d
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-15 12:00:00 +0530

    file added

--- 414-Third-max-number.js
+++ 414-Third-max-number.js
@@ -1,6 +1,11 @@
 // Creating a function thirdMax() and returning the third distinct maximum number in an array(nums[]).
-// input = [2,2,3,1]
-// ouput = 1
+// Input = [2,2,3,1]
+// Ouput = 1
+// Explanation:
+// The first distinct maximum is 3.
+// The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+// The third distinct maximum is 1.
+ 
 
 function thirdMax(nums){
   nums.sort(function (a, b) {
