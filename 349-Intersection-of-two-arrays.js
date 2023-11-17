3d8e99b7fd008df291696f140c0c04e10cd345d5
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-18 00:06:04 +0530

    File added

--- 349-Intersection-of-two-arrays.js
+++ 349-Intersection-of-two-arrays.js
@@ -3,17 +3,17 @@
 
 
 function intersect(nums1, nums2){
-    let i = 0;
+    let i = 0;         // Initializing i=0, so we can use this anywhere in a code.
     let newArr = [];  // Initializng an empty array to store values
 
     while(i<nums1.length){ // Using a while loop for one array(nums1)
         
-        for(let j = 0; j<nums2.length; j++){
+        for(let j = 0; j<nums2.length; j++){ // looping through the nums2 array by initializing j as a variable.
             if(nums1[i] == nums2[j]){
                 newArr.push(nums1[i]); // using push() method to add new values in new array(newArr)
             }
         }
-        i++; // increamenting the value of i
+        i++; // Increamenting the value of i
     }
     return newArr;   
 }
