29ae22e478d5c64b3af3036d01d3527d444dbdba
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-19 12:00:00 +0530

    file added

--- 349-Intersection-of-two-arrays.js
+++ 349-Intersection-of-two-arrays.js
@@ -10,13 +10,13 @@ function intersect(nums1, nums2){
         
         for(let j = 0; j<nums2.length; j++){ // looping through the nums2 array by initializing j as a variable.
             if(nums1[i] == nums2[j]){
-                Arr.push(nums1[i]); // using push() method to add new values in new array(newArr)
-                newArr = [...new Set(Arr)];
+                Arr.push(nums1[i]); // Using push() method to add new values in new array(newArr)
+                newArr = [...new Set(Arr)]; // Using set() to avoid duplicates.
             }
         }
         i++; // Increamenting the value of i
     }
-    return newArr;   
+    return newArr;   // Returning the final array
 }
 
 console.log(intersect([4,9,5],[9,4,9,8,4]));
