b98d81c51e7196a0b3a8080379e078f1dd5a2357
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-21 14:37:43 +0530

    File added

--- 283-Move-zeroes.js
+++ 283-Move-zeroes.js
@@ -1,6 +1,11 @@
+// Creating function moveZeroes() to move all the zeroes in array towards the end of array. 
+// Input = [0,1,0,3,12]
+// Output = [1,3,12,0,0];
+
+
 function moveZeroes(nums){
 
-    let i = 0;
+    let i = 0;  // Initilizing i and j variable as 0(You can give any name to this).
     let j = 0;
 
 
