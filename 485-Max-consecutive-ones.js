b003c8a18a08a17e9226e4cd3f5a36f2c423b12c
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-22 12:00:00 +0530

    file added

--- 485-Max-consecutive-ones.js
+++ 485-Max-consecutive-ones.js
@@ -2,22 +2,22 @@
 // Input = [1,1,0,1,1,1]
 // Output = 3
 
-function findConsecutiveOnes(nums){
-    let max = 0;
+function findConsecutiveOnes(nums){ // 
+    let max = 0;  // Initilizing max and current as a variable to add values later in code.
     let current = 0;
 
-     for(let i = 0; i< nums.length; i++){
-        if(nums[i] == 1){
-            current += 1;
+     for(let i = 0; i< nums.length; i++){  // looping through nums[] with variable i
+        if(nums[i] == 1){    // Using if else statements to check the different conditions 
+            current += 1;    // value of current will increamented only if the conditon is true
         } else{
-            current = 0;
+            current = 0;     // Else it will be zero
         }
         if(current>max){
-           max = current;
+           max = current;    // if the current value is greater than max then max = current. 
         }
      }
-     return max;
+     return max;             // finally returning max after the loop ends
 }
 
-console.log(findConsecutiveOnes([1,1,0,1,1,1]));
+console.log(findConsecutiveOnes([1,1,0,1,1,1])); // consoling and calling the function with nums.
 
