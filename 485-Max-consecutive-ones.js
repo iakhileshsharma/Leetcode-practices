2382b6fde1ea1aaa0723b9992fbf60d7786f9da5
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-23 15:18:10 +0530

    File added

--- 485-Max-consecutive-ones.js
+++ 485-Max-consecutive-ones.js
@@ -3,7 +3,20 @@
 // Output = 3
 
 function findConsecutiveOnes(nums){
+    let max = 0;
+    let current = 0;
 
+     for(let i = 0; i< nums.length; i++){
+        if(nums[i] == 1){
+            current += 1;
+        } else{
+            current = 0;
+        }
+        if(current>max){
+           max = current;
+        }
+     }
+     return max;
 }
 
 console.log(findConsecutiveOnes([1,1,0,1,1,1]));
