5a88f81e3bb5b0926a0ef9af28dd56e32b2141ad
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-25 12:00:00 +0530

    file added

--- 628-Maximum-productof-3-numbers.js
+++ 628-Maximum-productof-3-numbers.js
@@ -11,4 +11,4 @@ function maximumProduct(nums){
     let n = nums.length;        // initilizing n variable equals to length of an array
       return Math.max(nums[n-1] * nums[n-2] * nums[n-3], nums[0]* nums[1]*nums[n-1]);  // Math.max() method to compare between two cases here.
 }
-console.log(maximumProduct([-100,-98,-1,2,3,4])); // consoling and calling the function 
+console.log(maximumProduct([1,2,3,4])); // consoling and calling the function 
