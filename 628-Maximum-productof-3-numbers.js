46595103eb7f8e1f1856a6782ade30acb0d0f7f2
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-25 10:29:26 +0530

    File added

--- 628-Maximum-productof-3-numbers.js
+++ 628-Maximum-productof-3-numbers.js
@@ -3,5 +3,15 @@
 // Explanation : If we multiple 2*3*4 = 24 (which is maximum product of three number in an array).
 
 function maximumProduct(nums){
+    nums.sort(function (a, b) {
+        return a-b;
+      });
+      
+      let n = nums.length;
+      return Math.max(nums[n-1] * nums[n-2] * nums[n-3], nums[0]* nums[1]*nums[n-1]);
 
+
+
+   
 }
+console.log(maximumProduct([-100,-98,-1,2,3,4]));
