99e3f897dbd13467901bf71e03a1438d1ff1d839
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-25 12:00:00 +0530

    file added

--- 217-Contains-duplicate.js
+++ 217-Contains-duplicate.js
@@ -1,3 +1,7 @@
+// creating a function containsduplicate() with nums array to check if there is any duplicate element in array or not.
+// Input = [1,2,3,1]
+// Output = true
+
 function containsDuplicate(nums){
     nums.sort((a,b) => a-b);
     for(let i = 0; i <= nums.length-1; i++){
