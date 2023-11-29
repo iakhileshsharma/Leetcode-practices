9d4fb941c60b99b2db65bddc64981b0ac7da6829
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-11-29 19:19:22 +0530

    file added

--- 169-Majority-element.js
+++ 169-Majority-element.js
@@ -9,20 +9,20 @@ function majorityElement(nums){
   let lengthTosearch = nums.length /2;  // As given in the problem element should appear more than n/2.
 
   for(let i = 0; i<nums.length; i++){ // Looping through nums[] to iterate every element.
-    if(!obj[nums[i]]){
-      obj[nums[i]] = 0;
+    if(!obj[nums[i]]){                // If obj[nums[i]] is not in the obj euals it with zero.
+      obj[nums[i]] = 0;              
     }
 
-    obj[nums[i]]++;
+    obj[nums[i]]++;                   // if it appears more than one time than increament it by 1.
 
-    if(obj[nums[i]] >= lengthTosearch){
-      return nums[i];
+    if(obj[nums[i]] >= lengthTosearch){  // if particular key in obj is more than nums.length / 2
+      return nums[i];                    // return that key.
     }
   }
   return null;
    
 }
-console.log(majorityElement([3,2,3]));
+console.log(majorityElement([3,2,3])); // consoling and calling the function
 
 
 
