8634bbd719d805db1998df8b01fb8749638e0fd7
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-01 12:00:00 +0530

    file added

--- 704-Binary-search.js
+++ 704-Binary-search.js
@@ -1,9 +1,10 @@
 // Input: nums = [-1,0,3,5,9,12], target = 9
 // Output: 4
 // Explanation: 9 exists in nums and its index is 4
+// creating function called search( with parameters nums and target)
 
 
-function search(nums, target){          // creating function called search( with parameters nums and target)
+function search(nums, target){          
 
     for(let i = 0;i<nums.length; i++){ // Looping through nums[] to iterate over every element in array.
         if(nums[i] == target){         // Using if statment to check whether the target is in array or not.
