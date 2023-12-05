fb96efdd1da2d99295f035e89b4c12a537f95b1f
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-05 12:00:00 +0530

    file added

--- 268-Missing-Number.js
+++ 268-Missing-Number.js
@@ -7,19 +7,18 @@
 
 function missingNumber(nums){
    let obj = {};
-   let n = nums.length;
-   nums.sort((a,b)=> a-b);
+   let n = nums.length;     // Initilizing variable n equals to length of the nums[].
 
-   for(let el of nums){
+   for(let el of nums){     // Appliying forEach loop to iterate through nums with el.
     if(!obj[el]){
-        obj[el] =1;
+        obj[el] =1;         // If the given condition is true, then put obj[el] = 1.
     }
    }
-   for(let i = 0; i<=n; i++){
+   for(let i = 0; i<=n; i++){   // Another foor loop outside the previous loop to iterate upto the value of n.
     if(!obj[i]){
-        return i;
-    }
+        return i;               // Return the value of i is the given condition is true.
+    } 
    }
 }
 
-console.log(missingNumber([3,0,1]));
+console.log(missingNumber([3,0,1]));  // consoling and calling the function.
--- 287-Duplicate-Number.js
+++ 287-Duplicate-Number.js
@@ -1,14 +1,11 @@
-//Given an array of integers nums containing
-//  n + 1 integers where each integer 
-//  is in the range [1, n] inclusive.
-// Input: nums = [1,3,4,2,2]
-// Output: 2
-
+//Given an array of integers nums containing n + 1 integers where each integer 
+// is in the range [1, n] inclusive.
+// Input: nums = [1,3,4,2,2]  Output: 2
 
 function findDuplicate(nums){
     let obj = {};
-    let n = nums.length -1;
-
+    let n = nums.length - 1;
+    
     for(let i of nums){
         if(!obj[i]){
             obj[i] = 1;
@@ -17,11 +14,12 @@ function findDuplicate(nums){
         }
     }
     for(let j = 1; j <= n; j++){
-        if(obj[j] == 2){
+        if(obj[j] > 1){
             return j;
         }
     }
 }
+
 console.log(findDuplicate([1,3,4,2,2]));
 console.log(findDuplicate([3,1,3,4,2]));
 console.log(findDuplicate([1,1]));
