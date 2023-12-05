8012f7cd426c825d0b6a8d9c7439825a457f9dc1
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-05 12:00:00 +0530

    file added

--- 136-Single-Number.js
+++ 136-Single-Number.js
@@ -4,22 +4,22 @@
 // Output: 1
 
 function singleNumber(nums){
-  let obj = {};
+  let obj = {};               // Initilizing obj as an empty object
 
-  for(let i of nums){
-    if(!obj[i]){
+  for(let i of nums){         // Looping through nums with i.
+    if(!obj[i]){              // If the i element is not in obj, then give it a value of 1.
         obj[i] = 1;
     }else{
-        obj[i] += 1;
+        obj[i] += 1;          // If it is already there, then plus one the value.
     }
   }
-  for(let j = 0; j<nums.length; j++){
+  for(let j = 0; j<nums.length; j++){    // Another for loop with variable j to iterate upto length of nums.
     if(obj[nums[j]] == 1){
-        return nums[j];
+        return nums[j];                  // if the condition is true, then return the jth value from nums.
     }
   }
 }
 
-console.log(singleNumber([4,1,2,1,2]));
+console.log(singleNumber([4,1,2,1,2]));   // consoling and calling the function.
 console.log(singleNumber([2,2,1]));
 console.log(singleNumber([1]));
