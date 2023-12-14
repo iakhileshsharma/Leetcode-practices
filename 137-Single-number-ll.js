190c6112f545760cdb79ea3eb604dafb9011a916
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-14 16:22:30 +0530

    file added

--- 137-Single-number-ll.js
+++ 137-Single-number-ll.js
@@ -5,20 +5,20 @@
 // Output: 3
 
 function singleNumber(nums){
-let obj = {};            
+let obj = {};               // Initilizing obj as an empty object
 
-  for(let i of nums){         
-    if(!obj[i]){              
-        obj[i] = 1;
-    }else{
-        obj[i] += 1;        
+    for(let i of nums){         // Looping through nums with i.
+      if(!obj[i]){              // If the i element is not in obj, then give it a value of 1.
+          obj[i] = 1;
+      }else{
+          obj[i] += 1;          // If it is already there, then plus one the value.
+      }
     }
-  }
-  for(let j = 0; j<nums.length; j++){   
-    if(obj[nums[j]] == 1){
-        return nums[j];                  
+    for(let j = 0; j<nums.length; j++){    // Another for loop with variable j to iterate upto length of nums.
+      if(obj[nums[j]] == 1){
+          return nums[j];                  // if the condition is true, then return the jth value from nums.
+      }
     }
-  }
 }
-console.log(singleNumber([2,2,3,2]));
+console.log(singleNumber([2,2,3,2])); 
 
