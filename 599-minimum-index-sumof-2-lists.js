bfa60c7138b10a6f2a1cb36474b45fccea3469a8
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-17 18:18:18 +0530

    file added

--- 599-minimum-index-sumof-2-lists.js
+++ 599-minimum-index-sumof-2-lists.js
@@ -4,17 +4,6 @@
 // Explanation: The only common string is "Shogun".
 
 function findRestaurant(list1, list2){
- let i= 0;
- let indexSum = 0;
- let maxIndex = 0;
-    while(i<list1.length){
-        for(let j = 0; j,list2.length; j++){
-            if(list1[i] == list2[j]){
-               
-            }
-        }
-    i++;
-    }
 }
 
-console.log(findRestaurant());
+console.log(findRestaurant(["Shogun","Tapioca Express","Burger King","KFC"],["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]));
