8fcec662ba75d9554fd10685116f9f5128a8b5bb
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-17 12:00:00 +0530

    file added

--- 747-Largest-num-twice-of-others.js
+++ 747-Largest-num-twice-of-others.js
@@ -5,14 +5,13 @@
 // The index of value 6 is 1, so we return 1.
 
 function dominantIndex(nums){
-  let arr=[...nums];   //creates a new array by using spread syntax to copy elements from an existing elements.
+  let arr=[...nums];                             //creates a new array by using spread syntax to copy elements from an existing elements.
   arr.sort((a,b)=>a-b);
- if( arr[arr.length-1]>=arr[arr.length-2]*2){ // if statement to check the given condition true or not.
-     return nums.indexOf(arr[arr.length-1]);  // as given in the problem if condition true return index of the particular value.
+ if( arr[arr.length-1]>=arr[arr.length-2]*2){    // if statement to check the given condition true or not.
+     return nums.indexOf(arr[arr.length-1]);     // as given in the problem if condition true return index of the particular value.
  }else{
      return -1
  } 
 
 }
-console.log(dominantIndex([3,6,1,0])); //consoling and returning function.
-//dominantIndex([3,6,1,0]);
+console.log(dominantIndex([3,6,1,0]));            //consoling and returning function.
