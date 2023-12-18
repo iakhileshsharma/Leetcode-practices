3858e88f7bca15be58c75c0d85ad3d86c1951c95
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-18 12:00:00 +0530

    file added

---
+++ 442-Find-duplicate-in-array.js
@@ -0,0 +1,18 @@
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [2,3]
+
+function findDuplicates(nums){
+    let finalArr = [];
+    let obj = {};
+    let n = nums.length;
+
+    for(let el in nums){
+       if(!obj[el]){
+        obj[el] += 1;
+       }
+    }
+    for(let i = 0; i< n; i++){
+        
+    }
+}
+console.log(findDuplicates([4,3,2,7,8,2,3,1]));
