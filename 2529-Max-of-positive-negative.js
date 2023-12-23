42b56cbda1bcdfdf6cacbad388ba4714f7827db0
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-23 19:27:13 +0530

    file added

--- 2529-Max-of-positive-negative.js
+++ 2529-Max-of-positive-negative.js
@@ -2,19 +2,20 @@
 // Output: 3
 // Explanation: There are 3 positive integers and 3 negative integers. 
 // The maximum count among them is 3.
+// Note that 0 is neither positive nor negative.
 
 function maximumCount(nums){
-    let negCount = 0;
-    let posCount = 0;
+    let negCount = 0;          // Initilizing negCount to store number of negative integer in nums.
+    let posCount = 0;          // Initilizing posCount to store number of positive integers in nums.
     let result = 0;
-
-    for(let i = 0; i<nums.length; i++){
-        if(nums[i] < 0){
+  
+      for(let i = 0; i<nums.length; i++){
+          if(nums[i] < 0){
             negCount += 1;
-        } else if(nums[i] > 0){
+          } else if(nums[i] > 0){
             posCount += 1;
-        }
-    }
+          }
+      }
     result = Math.max(negCount, posCount);
     return result;
 }
