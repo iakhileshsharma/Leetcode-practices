49d1b0196497fc9234cba0ae1b29eb01c9f966f4
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-26 12:00:00 +0530

    file added

--- 1207-Unique-number-occurences.js
+++ 1207-Unique-number-occurences.js
@@ -2,7 +2,15 @@
 // Output: true
 
 function uniqueOccurrences(nums){
+    let obj = {};
     
+    for(let i of nums){           
+        if(!obj[i]){              
+            obj[i] = 1;             
+        }else{
+            obj[i] += 1;          
+        }
+      }
 }
 
 console.log(uniqueOccurrences([1,2,2,1,1,3]));
