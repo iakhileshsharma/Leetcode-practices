ccf1edb0f7313d7063c071eddf680ec059cf026b
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-30 13:17:45 +0530

    file added

--- 442-Find-duplicate-in-array.js
+++ 442-Find-duplicate-in-array.js
@@ -4,26 +4,28 @@
 // Output: [1]
 
 function findDuplicates(nums){
-    let finalArr = [];        //Initilizing empty array to final result
-    let obj = {};             // Initilizing empty object to use hashing concept.
-    let n = nums.length;      // Initilizing n which is equal to length of nums.
+    let finalArr = [];        
+    let obj = {};             
+    let n = nums.length;      
 
-    for(let el in nums){       // ForEach loop with el variable to iterate through nums.
-       if(!obj[el]){
-        obj[el] += 1;          // if the condition in line 10 is true, then add +1 value to obj[el].
-       }
+    for(let el of nums){     
+        if(!obj[el]){
+            obj[el] = 1;                
+        }else{
+            obj[el] += 1;
+        }
     }
-    for(let i = 0; i< n; i++){  // Simple for loop to iterate upto n.
-        if(obj[i]){
-           finalArr.push(obj[i]);
-           return finalArr;
-        }     
+    for(let i = 1; i<= n; i++){
+        if(obj[i] > 1){
+            finalArr.push(i);
+        }   
     }
+    return finalArr;
 }
 
-console.log(findDuplicates([4,3,2,7,8,2,3,1]));  //consoling and calling function.
-console.log(findDuplicates([1,1,2]));
-console.log(findDuplicates([1])); 
+//console.log(findDuplicates([4,3,2,7,8,2,3,1]));
+console.log(findDuplicates([2,2]));
+
 
 
 
