89a5dc9e21196a37879acf295d168e33bb4840c6
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2024-01-01 18:31:52 +0530

    file added

--- 2016-max-difference-in-inc-elements.js
+++ 2016-max-difference-in-inc-elements.js
@@ -1,20 +1,18 @@
 // All the given cases in problem
 // Input: nums = [7,1,5,4]
 // Output: 4
-// Input: nums = [1,5,2,10]
-// Output: 9
 // Input: nums = [9,4,3,2]
 // Output: -1
 
 function maximumDifference(nums) {
     let i = 0;
-    let max = 0;
-    let result = 0;
+    let max = 0;                     // Initiilizing max variable for further calculation in line 15.
+    let result = 0;                  
 
-    while(i < nums.length){
-       for(let j = i+1; j<nums.length; j++){
+    while(i < nums.length){         // While loop to iterate through nums upto the length.
+       for(let j = i+1; j<nums.length; j++){       // for loop inside the while with j variable to check condition between two elements of array.
         if(nums[i] < nums[j]){
-            max = nums[j] - nums[i];
+            max = nums[j] - nums[i];               
             result = Math.max(result, max);
         }
        }
