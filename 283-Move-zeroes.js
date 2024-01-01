9210449675ff37a1aba44a838496fcbcf9fd0d84
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2024-01-01 16:10:08 +0530

    file added

--- 283-Move-zeroes.js
+++ 283-Move-zeroes.js
@@ -3,20 +3,18 @@
 // Output = [1,3,12,0,0];
 
 
-function moveZeroes(nums){
+function moveZeroes(nums){           // function with nums
+let i = 0;                           // Initilizing i and j variable as 0(You can give any name to this).
+let j = 0;
 
-    let i = 0;  // Initilizing i and j variable as 0(You can give any name to this).
-    let j = 0;
-
-
-    while(i<nums.length){  // while loop with the condition of i must be smaller than length of nums.
-        if(nums[i]!=0){    // If statement for checking if the number is equal to zero or not. 
+    while(i<nums.length){               // while loop with the condition of i must be smaller than length of nums.
+        if(nums[i]!=0){                 // If statement for checking if the number is equal to zero or not. 
             [nums[i], nums[j]] = [nums[j], nums[i]];  // Elements will swap only if the condition is true.
-            j++;           // Increamenting only the value of j
+            j++;                        // Increamenting only the value of j
         }
-        i++;     // Increamenting value of i outside the if block
+        i++;                            // Increamenting value of i outside the if block
     }
-    return nums; // returning nums 
+    return nums;                        // returning nums outside ther while loop. 
 }
-console.log(moveZeroes([0,1,0,3,12]));
+console.log(moveZeroes([0,1,0,3,12])); //consoling and calling the function.
 
