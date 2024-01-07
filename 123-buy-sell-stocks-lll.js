4277f98526e8b1ab2984842b9751cd872943508e
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2024-01-07 12:00:00 +0530

    file added

--- 123-buy-sell-stocks-lll.js
+++ 123-buy-sell-stocks-lll.js
@@ -21,5 +21,5 @@ function maxProfit(nums){
     }
 
 console.log(maxProfit([3,3,5,0,0,3,1,4]));
-console.log(maxProfit([3,3,5,0,0,3,1,4]));
-console.log(maxProfit([3,3,5,0,0,3,1,4]));
+console.log(maxProfit([1,2,3,4,5]));
+console.log(maxProfit([7,6,4,3,1]));
