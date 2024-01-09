870213092d90df76bfb75fa579a1da3b7aa3abee
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2024-01-09 18:21:26 +0530

    file added

--- 123-buy-sell-stocks-lll.js
+++ 123-buy-sell-stocks-lll.js
@@ -6,7 +6,18 @@
 //  profit = 4-1 = 3.
 
 function maxProfit(nums){
+    let i = 0;                 // Initializing the value of i
+    let TotalProfit = 0;       // initializing variable to calculate final profit.
+    let profit = 0;
 
+      while(i<prices.length){                    // i must be lesser than the length of prices[]
+        let j = i+1;                             // Initializing j here because we want to increament both i and j at the same time.
+            if(prices[i] < prices[j]){
+              profit = prices[j] - prices[i];
+              TotalProfit = TotalProfit + prof
+            
 }
+      }
+    }
 
 console.log(maxProfit([3,3,5,0,0,3,1,4]));
