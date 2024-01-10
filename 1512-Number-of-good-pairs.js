b715b820e03fbb2015b5bf4f37864ebfdb98dcd6
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2024-01-10 12:00:00 +0530

    file added

--- 1512-Number-of-good-pairs.js
+++ 1512-Number-of-good-pairs.js
@@ -3,16 +3,16 @@
 
 
 function numIdenticalPairs(nums){
-let len = nums.length;
-let finalPair = 0;
+let len = nums.length;            // Initilizing len variable equals to length of the nums[].
+let finalPair = 0;                // Initilzing finalpair variable to store final value.
 
-  for(let i = 0; i < len ; i ++){
+  for(let i = 0; i < len ; i ++){      // Using for loop upto len with i variable.
       for(let j= i+1 ; j < len; j++){
            if(nums[i]===nums[j]) 
-           finalPair++;
+           finalPair++;                // Increamenting finalpair by plusone.
       }
   }
-  return finalPair;
+  return finalPair;                  // returning finalpair
 }
 
 console.log(numIdenticalPairs([1,2,3,1,1,3]));
