3106729b890fd0ddad8064aa6d6c729a6b21f57b
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2024-01-15 12:16:53 +0530

    file added

--- 1512-Number-of-good-pairs.js
+++ 1512-Number-of-good-pairs.js
@@ -3,9 +3,15 @@
 
 
 function numIdenticalPairs(nums){
-
+    let size=nums.length;
+    let  goodpair=0;
+  for(let i =0  ;i < size ;i ++){
+      for(let j=i+1 ; j <size;j++){
+           if(nums[i]===nums[j]) 
+           goodpair++;
+      }
+  }
+  return goodpair;
 }
 
 console.log(numIdenticalPairs([1,2,3,1,1,3]));
-console.log(numIdenticalPairs([1,1,1,1]));
-console.log(numIdenticalPairs([1,2,3]));
