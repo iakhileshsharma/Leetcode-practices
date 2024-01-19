103285ab1cf2496b13fc5815f143be1c23bab8b5
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2024-01-19 12:00:00 +0530

    file added

--- 1207-Unique-number-occurences.js
+++ 1207-Unique-number-occurences.js
@@ -20,5 +20,5 @@ function uniqueOccurrences(nums){
 }
 
 console.log(uniqueOccurrences([1,2,2,1,1,3]));
-console.log(uniqueOccurrences());
-console.log(uniqueOccurrences());
+console.log(uniqueOccurrences([1,2]));
+console.log(uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0]));
