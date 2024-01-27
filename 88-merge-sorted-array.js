0941fa6e4788c24cb1b0dda927992da30088f93e
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2024-01-27 12:00:00 +0530

    file added

--- 88-merge-sorted-array.js
+++ 88-merge-sorted-array.js
@@ -5,12 +5,12 @@ function merge(nums1, m, nums2, n){
     let i = m-1;                     // Initilizing i variable equals to m-1.
     let j = n-1;                     // Initilizing j variable equals to n-1.
     let k = m+n - 1;
-   
-    while(j >= 0){
+    
+    while(j >= 0){                   // using while loop with the condition.
         if(i >= 0 && nums1[i] > nums2[j]){
-            nums1[k--] = nums1[i--];
+            nums1[k--] = nums1[i--];       // if the condition in line 10 is true then this line will executes.
         }else {
-            nums1[k--] = nums2[j--];
+            nums1[k--] = nums2[j--];       // else this will execute
         }
     }
 
